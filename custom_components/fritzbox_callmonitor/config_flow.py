a883d84d2946076b467c3c15938191e5e4dc50ff
MeinedigitaleWelt <98622417+Th0tti@users.noreply.github.com>
2025-05-24 23:46:12 +0200

    Update config_flow.py

--- custom_components/fritzbox_callmonitor/config_flow.py
+++ custom_components/fritzbox_callmonitor/config_flow.py
@@ -3,11 +3,14 @@ from __future__ import annotations
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_PORT, CONF_USERNAME
+from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
 
 from .const import (
     DOMAIN,
-    DEFAULT_PORT,
+    DEFAULT_TR064_PORT,
+    DEFAULT_MONITOR_PORT,
+    CONF_TR064_PORT,
+    CONF_MONITOR_PORT,
     CONF_FETCH_CALL_HISTORY,
     CONF_FETCH_VOICEMAILS,
 )
@@ -17,7 +20,8 @@ DATA_SCHEMA = vol.Schema(
         vol.Required(CONF_HOST): str,
         vol.Required(CONF_USERNAME): str,
         vol.Required(CONF_PASSWORD): str,
-        vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
+        vol.Optional(CONF_TR064_PORT, default=DEFAULT_TR064_PORT): int,
+        vol.Optional(CONF_MONITOR_PORT, default=DEFAULT_MONITOR_PORT): int,
         vol.Optional(CONF_FETCH_CALL_HISTORY, default=True): bool,
         vol.Optional(CONF_FETCH_VOICEMAILS, default=False): bool,
     }
@@ -32,4 +36,3 @@ class FritzboxCallMonitorFlowHandler(ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             return self.async_create_entry(title="Fritz!Box Call Monitor", data=user_input)
         return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors=errors)
-
