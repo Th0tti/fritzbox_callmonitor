ddf2a531e122f832e0d6336b7da3d5c8fbedc8c3
MeinedigitaleWelt <98622417+Th0tti@users.noreply.github.com>
2025-05-24 21:28:19 +0200

    Update const.py

--- custom_components/fritzbox_callmonitor/const.py
+++ custom_components/fritzbox_callmonitor/const.py
@@ -1,1 +1,13 @@
+"""Konstanten für die Integration."""
+DOMAIN = "fritzbox_callmonitor"
+
+CONF_HOST = "host"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_PORT = "port"
+CONF_FETCH_CALL_HISTORY = "fetch_call_history"
+CONF_FETCH_VOICEMAILS = "fetch_voicemails"
+
+DEFAULT_PORT = 49000
+DEFAULT_UPDATE_INTERVAL = 3600  # Sekunden
 
