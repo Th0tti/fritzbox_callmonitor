d72ef04f50f2e05eb2d6a042ea33232c7c89bb87
MeinedigitaleWelt <98622417+Th0tti@users.noreply.github.com>
2025-05-25 09:19:10 +0200

    Update const.py

--- custom_components/Fritzbox_Anrufe/const.py
+++ custom_components/Fritzbox_Anrufe/const.py
@@ -1,1 +1,15 @@
+"""Konstanten für die Fritz!Box Anrufe Integration."""
+DOMAIN = "fritzbox_anrufe"
+
+CONF_HOST = "host"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_TR064_PORT = "tr064_port"
+CONF_MONITOR_PORT = "monitor_port"
+CONF_FETCH_CALL_HISTORY = "fetch_call_history"
+CONF_FETCH_VOICEMAILS = "fetch_voicemails"
+
+DEFAULT_TR064_PORT = 49000
+DEFAULT_MONITOR_PORT = 1012
+DEFAULT_UPDATE_INTERVAL = 3600  # Sekunden
 
